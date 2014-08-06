87579c6626d30ecd34368061f1d5d2716596b1f8
Antonio Pierro <antonio.pierro@gmail.com>
2014-08-06 18:44:56 +0200

    added useful comments

--- src/js/routerController.js
+++ src/js/routerController.js
@@ -5,20 +5,25 @@
 	define([
 		'backbone',
 		'app',
-		'views/mainView',
-		'collections/books'
-	], function (Backbone, app, MainView, BooksCollection) {
+		'views/chapterView',
+		'collections/books',
+		'collections/paragraphs'
+	], function (Backbone, app, ChapterView, BooksCollection, ParagraphsCollection) {
+
+		// TODO the collection should be not loaded here but by using events;
+		var collection = new ParagraphsCollection();
+		localStorage.clear();
 
 		return  {
 
 			doBook: function (title) {
-				console.log('book title', title, app);
 				require(['text!../media/' + title + '.htm'], function(textBook) {
-					app.mainRegion.show(new MainView({
+					app.mainRegion.show(new ChapterView({
 						model: new Backbone.Model({
 							'audio-book': 'src/media/' + title + '.mp3',
 							'text-book': textBook
-						})
+						}),
+						collection: collection
 					}));
 				});
 			},
@@ -26,6 +31,7 @@
 			doHome: function () {
 				app.books = new BooksCollection();
 				app.books.fetch({
+					// TODO add a new view to list all the titles
 					success: function () {
 						console.log('do home', app, app.books);
 					}
